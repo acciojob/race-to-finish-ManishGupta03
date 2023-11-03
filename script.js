c5466b7d08b429dc75972a67a480c59249cd193d
Manish Gupta <117648576+ManishGupta03@users.noreply.github.com>
2023-11-03 23:13:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ window.promises = [];
 
 // Do not change the code above this
 // add your promises to the array `promises`
-unction getRandomTime() {
+function getRandomTime() {
         return Math.floor(Math.random() * 5) + 1; // Random time between 1 and 5 seconds
       }
 
